a2db6e9b9fed469b91c1217af8770d83a5fdab81
Shuning Zhang <shuningz@usc.edu>
2020-12-09 13:33:37 -0800

    add exit after header

--- code/reminders.php
+++ code/reminders.php
@@ -3,6 +3,7 @@ require "config/config.php";
 session_start();
 if( !isset($_SESSION["logged_in"]) || !$_SESSION["logged_in"] ) {
 	header("Location: login.php");
+	exit();
 }
 else {
 	$userid = $_SESSION["userid"];
